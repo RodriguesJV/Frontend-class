48c258ddceba16daa82e3ec9df4fc12c9e23ef72
RodriguesJV <25114290126@ACADEMICO.IESB.BR>
2026-03-20 21:45:21 -0300

    update

---
+++ praticas/pratica04/chronos-pomodoro/src/App.tsx
@@ -0,0 +1,10 @@
+import { Heading } from "./Components/Heading"; 
+import "./style/theme.css";                
+import "./style/global.css";                 
+
+
+export function App(){
+    return <Heading/>
+    
+    
+}
