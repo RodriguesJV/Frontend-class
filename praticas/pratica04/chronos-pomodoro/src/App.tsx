638dd3f8370b636c7d184d420ebdbaec8053df32
RodriguesJV <joaovit3625@gmail.com>
2026-03-27 17:56:25 -0300

    update - src

--- praticas/pratica04/chronos-pomodoro/src/App.tsx
+++ praticas/pratica04/chronos-pomodoro/src/App.tsx
@@ -1,4 +1,13 @@
-export function App() {
-  return <h1>Olá, Mundo!</h1>;
+// 1º: Importamos as variáveis (o tema)
+import './styles/theme.css';
+// 2º: Importamos os estilos globais
+import './styles/global.css';
 
+export function App() {
+  return (
+    <>
+      <h1>Olá, Mundo!</h1>
+      <p>Testando o CSS Global.</p>
+    </>
+  );
 }
