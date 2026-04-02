88f850adabeea1f9e301677d6f5bcf620d5957c9
RodriguesJV <joaovit3625@gmail.com>
2026-04-02 20:06:27 -0300

    update ajustes

--- praticas/pratica04/chronos-pomodoro/src/App.jsx
+++ praticas/pratica04/chronos-pomodoro/src/App.jsx
@@ -1,6 +1,12 @@
 import { Container } from './components/Container';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
+import { CountDown } from './components/CountDown';
+import { DefaultInput } from './components/DefaultInput';
+import { Cycles } from './components/Cycles';
+import { DefaultButton } from './components/DefaultButton';
+import { PlayCircleIcon } from 'lucide-react';
+import { Footer } from './components/Footer';
 
 import './styles/theme.css';
 import './styles/global.css';
@@ -11,9 +17,41 @@ export function App() {
       <Container>
         <Logo />
       </Container>
-
       <Container>
         <Menu />
+      </Container>
+      <Container>
+        <CountDown />
+      </Container>
+
+      <Container>
+        <form className='form' action=''>
+          <div className='formRow'>
+            {/* Voltamos o labelText para 'task' */}
+            <DefaultInput
+              labelText='task'
+              id='meuInput'
+              type='text'
+              placeholder='Digite algo'
+            />
+          </div>
+
+          <div className='formRow'>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div className='formRow'>
+            <Cycles />
+          </div>
+
+          <div className='formRow'>
+            <DefaultButton icon={<PlayCircleIcon />} />
+          </div>
+        </form>
+      </Container>
+
+      <Container>
+        <Footer />
       </Container>
     </>
   );
